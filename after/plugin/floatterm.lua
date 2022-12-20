1999727bb783071b7525a7e415780aff78510a8b
j33pguy <jeepguygaming@gmail.com>
2022-12-20 15:38:07 -0500

    working on floaterm

---
+++ after/plugin/floatterm.lua
@@ -0,0 +1,8 @@
+vim.keymap.set("n","<leader>t",":FloatermNew<CR>")
+vim.keymap.set("n","<leader>tk",":FloatermKill<CR>")
+vim.keymap.set("n","<leader>th",":FloatermHide<CR>")
+vim.keymap.set("n","<leader>tt","<C-\\><C-n>:FloatermToggle<CR>")
+vim.keymap.set("n","<leader>tn",":FloatermNext<CR>")
+vim.keymap.set("n","<leader>tp",":FloatermPrev<CR>")
+
+vim.g.floaterm_winblend = 50
--- after/plugin/statusline.lua
+++ after/plugin/statusline.lua
@@ -1,1 +1,3 @@
 vim.opt.statusline = "%#warningmsg#"
+
+vim.g["airline#extensions#tabline#enabled"] = 1
